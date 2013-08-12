9056de12fe8cf76b2228d7ea538a717946fdc6b8
Joao Azevedo <joao.c.azevedo@gmail.com>
2013-08-12 14:00:43 +0100

    Add some space for >> operator.

--- src/game-of-thrones.cc
+++ src/game-of-thrones.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     char in;
     bitset<26> A;
-    while (cin>>in)
+    while (cin >> in)
         A.flip(in-'a');
     cout << (A.count() <= 1 ? "YES" : "NO") << endl;
     return 0;
