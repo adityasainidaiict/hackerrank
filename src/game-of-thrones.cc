85c2b22ab607166d88adbc7d00ff52956837881b
Joao Azevedo <joao.c.azevedo@gmail.com>
2013-08-12 14:20:17 +0100

    Add some operator space.

--- src/game-of-thrones.cc
+++ src/game-of-thrones.cc
@@ -7,7 +7,7 @@ int main() {
     char in;
     bitset<26> A;
     while (cin >> in)
-        A.flip(in-'a');
+        A.flip(in - 'a');
     cout << (A.count() <= 1 ? "YES" : "NO") << endl;
     return 0;
 }
